360b9f4b472daa4cbcc808a8da1f6463979f22c9
plugshubham <shbhkumar6@gmail.com>
2017-10-18 15:28:47 +0530

    this is for version second

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 This is Typing speed Test Application using HTML,CSS and Javascript. This is ver1 for this application. 
 I have used basic javascript DOM concept to develop this application
 This is open source so anyone can help in increasing it functionality
+
+
+ver2
+What's new?
+
+1.Accuracy calculation
+2.Reset Functionality
+3.Better CSS
--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-    var counter=0;
+    const startbutton=document.getElementById("start");
+    const stopbutton=document.getElementById("stop");
+    const hinttext=document.getElementById("hint-area");
+    let hinttextvalue;
+    function resetText(){
+        document.getElementById("typingArea").value="";
+        
+        //console.log(val);
+    }
+    function showText(){
+        
+        hinttextvalue=hinttext.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non fringilla metus. Sed nec euismod velit. Phasellus venenatis vitae justo ac feugiat. Proin suscipit leo felis, id tristique tortor semper id. Pellentesque risus magna, efficitur ut ex ut, tristique vestibulum est. Integer semper porta felis. Integer neque diam, imperdiet vitae mi in, tempus placerat ipsum. Quisque ut enim vitae tellus varius feugiat et sit amet lacus. Cras tortor mauris, lacinia ut pharetra ac, posuere non neque. Morbi gravida, velit commodo posuere sollicitudin, orci erat ultrices magna, sit amet hendrerit nulla mi non nibh. Vivamus vel auctor tellus, vitae lacinia mauris.";
+        
+    }
+    var counter=1;
+    var count1=0;
     var start;
-    var stop;    
+    var stop;  
+    function resetText(){
+        document.getElementById("typingArea").value="";
+        counter=0;
+        //console.log(val);
+    }
     function startTime(){
         var date =new Date();
         start=((date.getMinutes())*60)+date.getSeconds();
@@ -10,13 +30,42 @@
     function testSpeed(){
         if(event.keyCode==32){
             counter++;
-            document.getElementById("showSpeed").innerHTML="WORDS:"+counter;
+            //document.getElementById("showSpeed").innerHTML="WORDS:"+counter;
         }
     }
     
-    function stopTime(){
+    function calAccuracy(){
+        if((document.getElementById("typingArea").value)==""){
+            document.getElementById("result").innerHTML="";
+            alert ("enter the text first")
+        }
+        else{
         var date =new Date();
         stop=((date.getMinutes())*60)+date.getSeconds();
-        var resultValue= Math.round(counter/((stop-start)/60));
-        document.getElementById("result").innerHTML="<h3>Total Seconds:  "+(stop-start)+"<br>"+"Final Speed:  "+resultValue+" words/min</h3>";
+        var text1=hinttextvalue.split(" ");
+        //console.log(text1);
+        var text2=(document.getElementById("typingArea").value).split(" ");
+        //console.log(text2);
+        var textlength=text2.length;
+        //console.log(textlength);
+        for(var i=0;i<textlength;i++){
+            if(text1[i]!=text2[i]){
+                count1++;
+            }
+        }
+        var resultValue= Math.round((counter+1)/((stop-start)/60));
+        //console.log(counter+1+"this is textarea value");
+        //console.log(count1+"this is value of count");
+        var accuracy= Math.round((((counter+1)-count1)/(counter+1))*100);
+        //console.log(accuracy);
+        document.getElementById("result").innerHTML="<h3>Total Seconds:  "+(stop-start)+"<br>"+"Final Speed:  "+resultValue+" words/min<br>"
+            +"Accuracy:"+accuracy+" %</h3>";
+        }
     }
+    startbutton.addEventListener("click",startTime);//start the timer
+    startbutton.addEventListener("click",showText);// show text in hint area
+    startbutton.addEventListener("click",resetText);// reset text area
+    startbutton.addEventListener("click",function(){
+        document.getElementById("stop").style.display="inline";//display stop button
+    });
+    stopbutton.addEventListener("click",calAccuracy);//result
